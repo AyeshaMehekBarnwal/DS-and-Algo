60bd72fd3333b402de2d5f269f7e666c194a92e8
AyeshaMehekBarnwal <ayeshagoingahead@outlook.com>
2020-10-22 12:02:17 +0530

    search

---
+++ linked-list/searchElement.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *next;
+};
+
+Node * GetNode(int newData)
+{
+	Node *newNode = new Node();
+	newNode->data = newData;
+	newNode->next = NULL;
+}
+
+void InsertAtBeginning(Node **head, int newData)
+{
+	Node *newNode = GetNode(newData);
+	newNode->next = *head;
+	*head = newNode;
+}
+
+int findLengthIterative(Node *head)
+{
+	int count = 0;
+	
+	while(head != NULL)
+	{
+		cout << head->data << endl;
+		head = head->next;
+		count++;
+	}
+
+	return count;
+}
+
+bool searchElementIterative(Node *head, int key)
+{
+	if(head == NULL)
+		return false;
+
+	while(head != NULL)
+	{
+		if(head->data == key)
+			return true;
+		head = head->next;
+	}
+
+	return false;
+}
+
+int main()
+{
+	Node *head = NULL;
+	int key = 67;
+	InsertAtBeginning (&head, 7);
+	InsertAtBeginning (&head, 1);
+	InsertAtBeginning (&head, 12);
+	InsertAtBeginning (&head, 5);
+	InsertAtBeginning (&head, 8);
+	InsertAtBeginning (&head, 2);
+	InsertAtBeginning (&head, 6);
+	InsertAtBeginning (&head, 9);
+	InsertAtBeginning (&head, 0);
+
+	int noOfNodes = findLengthIterative(head);
+	cout << "Total number of nodes : " << noOfNodes << endl;
+
+	searchElementIterative(head, key) ? cout << "The number is present!" << endl : cout << "The number is not present!" << endl;
+
+	return 0;
+}
