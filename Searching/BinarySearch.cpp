8b59da5a160ae8a2d58957b66073ea2a0118ab43
AyeshaMehekBarnwal <ayeshagoingahead@outlook.com>
2020-12-01 18:20:55 +0530

    binary search

--- Searching/BinarySearch.cpp
+++ Searching/BinarySearch.cpp
@@ -24,13 +24,37 @@ int binarySearchItr(int arr[], int n, int key)
 	return -1;
 }
 
+int binarySearchRec(int arr[], int l, int r, int key)
+{
+	if(l <= r)
+	{
+		int mid = l + (r - l) / 2;
+		if(arr[mid] == key)
+			return mid;
+
+		if(arr[mid] > key)
+			return binarySearchRec(arr, 0, mid - 1, key);
+		else
+			return binarySearchRec(arr, mid + 1, r, key);
+	}
+
+	return -1;
+}
+
 int main()
 {
 	int arr[] = { 2, 3, 4, 10, 40 }; 
     int key = 10; 
     int n = sizeof(arr) / sizeof(arr[0]); 
     int result = binarySearchItr(arr, n, key); 
+	int res = binarySearchRec(arr, 0, n - 1, key);
     (result == -1) ? cout << "Element is not present in array"
-                   : cout << "Element is present at index " << result; 
+                   : cout << "Element is present at index " << result << endl;
+
+	(res == -1) ? cout << "Element is not present in array"
+                   : cout << "Element is present at index " << res << endl;
+
+
+	 
     return 0;
 }
